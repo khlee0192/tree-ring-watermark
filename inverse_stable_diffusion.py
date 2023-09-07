e5fb3269c3d40b91f6b86c9f0ac7b841375d4dc4
khlee0192 <khlee0192@gmail.com>
2023-09-07 21:51:01 +0900

    first edit

--- inverse_stable_diffusion.py
+++ inverse_stable_diffusion.py
@@ -55,7 +55,7 @@ class InversableStableDiffusionPipeline(ModifiedStableDiffusionPipeline):
                 feature_extractor,
                 requires_safety_checker)
 
-        self.forward_diffusion = partial(self.backward_diffusion, reverse_process=True)
+        #self.forward_diffusion = partial(self.backward_diffusion, reverse_process=True)
     
     def get_random_latents(self, latents=None, height=512, width=512, generator=None):
         height = height or self.unet.config.sample_size * self.vae_scale_factor
@@ -101,9 +101,95 @@ class InversableStableDiffusionPipeline(ModifiedStableDiffusionPipeline):
         latents = encoding * 0.18215
         return latents
 
-
     @torch.inference_mode()
     def backward_diffusion(
+        self,
+        use_old_emb_i=25,
+        text_embeddings=None,
+        old_text_embeddings=None,
+        new_text_embeddings=None,
+        latents: Optional[torch.FloatTensor] = None,
+        num_inference_steps: int = 20,
+        guidance_scale: float = 7.5,
+        callback: Optional[Callable[[int, int, torch.FloatTensor], None]] = None,
+        callback_steps: Optional[int] = 1,
+        reverse_process: True = False,
+        **kwargs,
+    ):
+        """ Generate image from text prompt and latents
+        """
+        # here `guidance_scale` is defined analog to the guidance weight `w` of equation (2)
+        # of the Imagen paper: https://arxiv.org/pdf/2205.11487.pdf . `guidance_scale = 1`
+        # corresponds to doing no classifier free guidance.
+        do_classifier_free_guidance = guidance_scale > 1.0
+        # set timesteps
+        self.scheduler.set_timesteps(num_inference_steps)
+        # Some schedulers like PNDM have timesteps as arrays
+        # It's more optimized to move all timesteps to correct device beforehand
+        timesteps_tensor = self.scheduler.timesteps.to(self.device)
+        # scale the initial noise by the standard deviation required by the scheduler
+        latents = latents * self.scheduler.init_noise_sigma
+
+        if old_text_embeddings is not None and new_text_embeddings is not None:
+            prompt_to_prompt = True
+        else:
+            prompt_to_prompt = False
+
+
+        for i, t in enumerate(self.progress_bar(timesteps_tensor if not reverse_process else reversed(timesteps_tensor))):
+            if prompt_to_prompt:
+                if i < use_old_emb_i:
+                    text_embeddings = old_text_embeddings
+                else:
+                    text_embeddings = new_text_embeddings
+
+            # expand the latents if we are doing classifier free guidance
+            latent_model_input = (
+                torch.cat([latents] * 2) if do_classifier_free_guidance else latents
+            )
+            latent_model_input = self.scheduler.scale_model_input(latent_model_input, t)
+
+            # predict the noise residual
+            noise_pred = self.unet(
+                latent_model_input, t, encoder_hidden_states=text_embeddings
+            ).sample
+
+            # perform guidance
+            if do_classifier_free_guidance:
+                noise_pred_uncond, noise_pred_text = noise_pred.chunk(2)
+                noise_pred = noise_pred_uncond + guidance_scale * (
+                    noise_pred_text - noise_pred_uncond
+                )
+
+            prev_timestep = (
+                t
+                - self.scheduler.config.num_train_timesteps
+                // self.scheduler.num_inference_steps
+            )
+            # call the callback, if provided
+            if callback is not None and i % callback_steps == 0:
+                callback(i, t, latents)
+            
+            # ddim 
+            alpha_prod_t = self.scheduler.alphas_cumprod[t]
+            alpha_prod_t_prev = (
+                self.scheduler.alphas_cumprod[prev_timestep]
+                if prev_timestep >= 0
+                else self.scheduler.final_alpha_cumprod
+            )
+            if reverse_process:
+                alpha_prod_t, alpha_prod_t_prev = alpha_prod_t_prev, alpha_prod_t
+            latents = backward_ddim(
+                x_t=latents,
+                alpha_t=alpha_prod_t,
+                alpha_tm1=alpha_prod_t_prev,
+                eps_xt=noise_pred,
+            )
+
+        return latents
+
+    @torch.inference_mode()
+    def forward_diffusion(
         self,
         use_old_emb_i=25,
         text_embeddings=None,
